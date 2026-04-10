20891d3a9083df1f0555653bf628a971cb48ffaf
julyanegarcia <julyanegarcia@id.uff.br>
2026-04-10 11:31:18 -0300

    Script esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,15 @@
+# Leitura do banco de dados
+
+dados = read.csv("imoveis.csv", header=T, sep=";")
+
+# Visualização inicial
+head(dados)
+str(dados)
+
+# Gráficos
+
+# (em construção)
+
+# Medidas
+
+# (em construção)
